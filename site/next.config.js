e08f54cb03b8f84bd569e823236ffb5ead9e5e50
tonyhsu2719 <tonyhsu@metaage.com.tw>
2024-10-24 19:04:23 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    icon: 'https://tonyhsu2719.github.io/kasmrepo/1.0/icon.png',
+    listUrl: 'https://tonyhsu2719.github.io/kasmrepo/',
+    contactUrl: 'https://github.com/tonyhsu2719/kasmrepo/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasmrepo/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
